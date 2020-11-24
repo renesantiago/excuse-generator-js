ef7663b2c0ae534aa28b273b9cf61a0e0f0ba745
renesantiago <rene.a.santiago@gmail.com>
2020-11-24 04:00:05 +0000

    Initial commit.

--- src/main.js
+++ src/main.js
@@ -6,5 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = getRandomInteger(0, who.length - 1);
+  let randomAction = getRandomInteger(0, action.length - 1);
+  let randomWhat = getRandomInteger(0, what.length - 1);
+  let randomWhen = getRandomInteger(0, when.length - 1);
+
+  excuse.innerHTML =
+    who[randomWho] +
+    " " +
+    action[randomWhat] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
 };
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
